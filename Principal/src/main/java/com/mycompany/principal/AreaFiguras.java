5b0ba3a85a17edef0b6b1a6571363a5456e98075
Jose Buendia Polo <jbuendia248@ieszaidinvergeles.org>
2022-05-27 13:23:50 +0200

    Creado el codigo de la clase

--- Principal/src/main/java/com/mycompany/principal/AreaFiguras.java
+++ Principal/src/main/java/com/mycompany/principal/AreaFiguras.java
@@ -9,5 +9,29 @@ package com.mycompany.principal;
  * @author Administrador
  */
 public class AreaFiguras {
-    
+
+    double area;
+
+    /*Métodos que permiten calcular las areas, estos reciben
+      como parámetros el lado, base, altura y radio, dependiendo cada caso.
+     */
+    public double areaCuadrado(double lado) {
+        area = lado * lado;
+        return area;
+    }
+
+    public double areaRectangulo(double base, double altura) {
+        area = base * altura;
+        return area;
+    }
+
+    public double areaTriangulo(double base, double altura) {
+        area = (base * altura) / 2;
+        return area;
+    }
+
+    public double areaCirculo(double radio) {
+        area = Math.PI * radio * radio;
+        return area;
+    }
 }

dbb62f07fca445aa02c199064bf8ce00a91240be
Jose Buendia Polo <jbuendia248@ieszaidinvergeles.org>
2022-05-27 13:20:54 +0200

    Creado el codigo

--- Principal/src/main/java/com/mycompany/principal/Principal.java
+++ Principal/src/main/java/com/mycompany/principal/Principal.java
@@ -5,6 +5,8 @@
 
 package com.mycompany.principal;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Administrador
@@ -12,6 +14,30 @@ package com.mycompany.principal;
 public class Principal {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        AreaFiguras af = new AreaFiguras(); //Instanciamos la clase "AreaFiguras"
+        Scanner sc = new Scanner(System.in); //Instanciamos la clase Scanner
+
+        double lado, altura, base, radio; //Variables de tipo double
+        System.out.print("Ingrese el lado del cuadrado: "); //Pedimos el lado por teclado
+        lado = sc.nextDouble(); //Almacenamos el lado en la variable lado
+        System.out.print("Ingrese la base del triangulo: ");
+        base = sc.nextDouble();
+        System.out.print("Ingrese la altura del triangulo: ");
+        altura = sc.nextDouble();
+        System.out.print("Ingrese la base del rectangulo: ");
+        base = sc.nextDouble();
+        System.out.print("Ingrese la altura del rectangulo: ");
+        altura = sc.nextDouble();
+        System.out.print("Ingrese el radio del circulo: ");
+        radio = sc.nextDouble();
+        /*
+         * En las siguientes lineas invocamos los diferentes métodos que
+         * trae nuestro objeto "af" y mostramos el resultado por consola.
+         */
+        System.out.println("------------------------------------------");
+        System.out.println("El area del cuadrado es: " + af.areaCuadrado(lado));
+        System.out.println("El area del triangulo es: " + af.areaTriangulo(base, altura));
+        System.out.println("El area del rectangulo es: " + af.areaRectangulo(base, altura));
+        System.out.println("El area del circulo es: " + af.areaCirculo(radio));
     }
 }
